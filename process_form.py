bc4f1ec79ebf4f5d5d762592abf87d8c3a39ea15
fofrafo <Fo@francisco-otto.de>
2024-01-09 01:14:50 -0300

    commit hcaptcha config 1

--- process_form.py
+++ process_form.py
@@ -1,37 +1,110 @@
+# import smtplib
+# from email.mime.text import MIMEText
+# from email.mime.multipart import MIMEMultipart
+# from flask import Flask, render_template, request, redirect, url_for
+# from decouple import config
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html', success_message=None)
+
+# @app.route('/process_form', methods=['GET', 'POST'])
+# def procesar_formulario():
+    
+#     if request.method == 'POST':    
+#         name = request.form['name']
+#         email = request.form['email']
+#         message = request.form['message']
+        
+#         # Accede a las variables de entorno
+#         gmail_user = str(config('GMAIL_USERNAME'))
+#         gmail_password = str(config('GMAIL_PASSWORD'))
+        
+#         # Datos del servidor SMTP
+#         smtp_server = 'smtp.gmail.com'
+#         smtp_port = 587
+#         smtp_user = gmail_user
+#         smtp_password = gmail_password
+        
+#         # Datos del formulario
+#         # name = input('Name: ')
+#         # email = input('Email: ')
+#         # message = input('Message: ')
+
+#         # Crear mensaje de correo
+#         msg = MIMEMultipart()
+#         msg['From'] = email
+#         msg['To'] = smtp_user
+#         msg['Subject'] = f'New contact message from {name}'
+
+#         body = f'Name: {name}\nEmail: {email}\nMessage: {message}'
+#         msg.attach(MIMEText(body, 'plain'))
+
+#         # Conectar al servidor SMTP y enviar correo
+#         try:
+#             server = smtplib.SMTP(smtp_server, smtp_port)
+#             server.starttls()
+#             server.login(smtp_user, smtp_password)
+#             server.sendmail(smtp_user, smtp_user, msg.as_string())
+#             server.quit()
+#             print('Mail sent successfully')
+#         except Exception as e:
+#             print(f'Error to send email: {str(e)}')
+#     print('Formulario enviado correctamente')
+        
+#     return redirect('/#contact-title')
+
+# if __name__ == '__main__':
+#     app.run()
+
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+import requests
 from decouple import config
+import logging
 
 app = Flask(__name__)
+
+# Configurar el sistema de logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def verificar_hcaptcha(response):
+    secret_key = str(config('HCAPTCHA_SECRET_KEY'))
+    verification_url = f"https://hcaptcha.com/siteverify?secret={secret_key}&response={response}"
+    verification_result = requests.post(verification_url)
+    data = verification_result.json()
+    return data.get('success', False)
+
 
 @app.route('/')
 def index():
     return render_template('index.html', success_message=None)
 
-@app.route('/process_form', methods=['GET', 'POST'])
+
+@app.route('/process_form', methods=['POST'])
 def procesar_formulario():
-    
-    if request.method == 'POST':    
-        name = request.form['name']
-        email = request.form['email']
-        message = request.form['message']
-        
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+    h_captcha_response = request.form.get('h-captcha-response')
+
+    # Verificar hCaptcha
+    # Verificar hCaptcha
+    if verificar_hcaptcha(h_captcha_response):
         # Accede a las variables de entorno
         gmail_user = str(config('GMAIL_USERNAME'))
         gmail_password = str(config('GMAIL_PASSWORD'))
-        
+
         # Datos del servidor SMTP
         smtp_server = 'smtp.gmail.com'
         smtp_port = 587
         smtp_user = gmail_user
         smtp_password = gmail_password
-        
-        # Datos del formulario
-        # name = input('Name: ')
-        # email = input('Email: ')
-        # message = input('Message: ')
 
         # Crear mensaje de correo
         msg = MIMEMultipart()
@@ -49,12 +122,16 @@ def procesar_formulario():
             server.login(smtp_user, smtp_password)
             server.sendmail(smtp_user, smtp_user, msg.as_string())
             server.quit()
-            print('Mail sent successfully')
+            logger.info('Mail sent successfully')
         except Exception as e:
-            print(f'Error to send email: {str(e)}')
-    print('Formulario enviado correctamente')
-        
-    return redirect('/#contact-title')
+            logger.error(f'Error to send email: {str(e)}')
+
+        logger.info('Formulario enviado correctamente')
+        return jsonify({'success': 'Formulario enviado correctamente.'}), 200
+    else:
+        logger.warning('Error: Por favor, verifica que eres humano.')
+        return jsonify({'error': 'Error: Por favor, verifica que eres humano.'}), 400
+
 
 if __name__ == '__main__':
     app.run()
