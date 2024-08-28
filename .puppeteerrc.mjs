48b2f0c6cc3439d754996284a599af2dabd6defc
Ramon Serrano <ramonserrano76@gmail.com>
2024-08-28 19:53:39 -0300

    commit 28-8-24_11

--- .puppeteerrc.mjs
+++ .puppeteerrc.mjs
@@ -5,11 +5,8 @@ import { __dirname } from './app.js';
  * @type {import("puppeteer").Configuration}
  */
 
-// const __dirname = path.dirname(new URL(import.meta.url).pathname).substring(1);
-// console.log('DIRNAME:', __dirname);
-
 // Define la ruta de la caché
-const cacheDirectory = path.join('.cache', 'puppeteer');
+const cacheDirectory = path.join(__dirname, '.cache', 'puppeteer');
 
 // Crea la carpeta de caché si no existe
 if (!existsSync(cacheDirectory)) {
